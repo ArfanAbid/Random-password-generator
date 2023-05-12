a23339cae48f91d6eab9e1deda1f419ab3f5bd71
Arfan Abid <131770875+ArfanAbid@users.noreply.github.com>
2023-05-12 13:30:01 +0500

    Add files via upload

---
+++ Random password.py
@@ -0,0 +1,25 @@
+import random
+import string
+
+def generate_password(length):
+    """
+    Generate a random password of specified length
+    """
+    # Define the character sets to use in the password
+    lowercase = string.ascii_lowercase
+    uppercase = string.ascii_uppercase
+    numbers = string.digits
+    
+    # Combine the character sets into a single set of possible characters
+    possible_chars = lowercase + uppercase + numbers
+    # Generate a random password by selecting characters from the possible set
+    password = ""
+    for i in range(length):
+        password =password + random.choice(possible_chars)
+    
+    return password
+
+
+length=int(input("Enter the size of password :"))
+password = generate_password(length)
+print("The password generated is:" ,password)
